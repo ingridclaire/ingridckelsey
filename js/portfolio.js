a57508eff9fb528c9c93b6ec2a65e8140c36ddd9
Ingrid Carlson <ingridclaire@gmail.com>
2021-08-20 11:25:40 -0400

    adjusts margin/padding within portfolio card

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,7 @@
 const projects = [
   {
     company: 'Doulas By The Bay',
+    category: 'web development',
     title: 'Doulas By The Bay website design',
     expertise: 'full-stack web development',
     tech: 'React, Redux, Node.js, MongoDB',
@@ -10,5 +11,21 @@ const projects = [
     id: '',
     image: '',
     description: ''
+  },
+  {
+    company: "The Illustrated Enby",
+    category: 'shopify store set-up',
+    title: "The Illustrated Enby - Shopify Store Set-Up",
+    expertise: "Shopify store set-up, theme customization, and custom code",
+    tech: 'Shopify, JavaScript',
+    deliverables: 'Shopify store',
+    website: 'www.theillustratedenby.com',
+    github: '',
+    id: '',
+    image: '',
+    description: "This Shopify store highlights the artist's original work, and makes prints available as well."
+  },
+  {
+
   }
 ]
