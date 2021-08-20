40955a6075113253eb916bb61c55e214d0967543
Ingrid Carlson <ingridclaire@gmail.com>
2021-08-19 20:57:18 -0400

    added custom js file with project data and linked proj card a tags to portfolio detail html page

---
+++ js/portfolio.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    company: 'Doulas By The Bay',
+    title: 'Doulas By The Bay website design',
+    expertise: 'full-stack web development',
+    tech: 'React, Redux, Node.js, MongoDB',
+    deliverables: 'complete website, database design, ui screens, ux flow',
+    website: 'www.doulasbythebay.com',
+    github: '',
+    id: '',
+    image: '',
+    description: ''
+  }
+]
